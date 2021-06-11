31a9927614dcb7e799abc78b23c4322973e0dbe2
TKB <65678581+TKBirrer@users.noreply.github.com>
2021-06-11 15:40:38 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,30 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    titlePanel("Hello World!"),
-    
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            textInput(inputId = "text_input", label = "Input text here:")
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
         ),
-        
+
+        # Show a plot of the generated distribution
         mainPanel(
-            textOutput(outputId = "text_output")
+           plotOutput("distPlot")
         )
     )
 )
 
+# Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-    text <- reactive({
-        paste("You typed:", input$text_input)
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
-    
-    output$text_output <- renderText({
-        text()
-    })
-    
 }
 
+# Run the application 
 shinyApp(ui = ui, server = server)
